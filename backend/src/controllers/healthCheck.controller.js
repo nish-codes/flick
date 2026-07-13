aa04a809dba3b4c2f46ac8935140fa5fe729aca0
nish-codes <nishanth24106@gmail.com>
2026-07-13 22:31:54 +0530

    revert: restore health check to 200

--- backend/src/controllers/healthCheck.controller.js
+++ backend/src/controllers/healthCheck.controller.js
@@ -2,7 +2,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asynchHandler.js";
 
 const heatlthCheck = asyncHandler(async(req,res)=>{
-    res.status(500).json(new ApiResponse(500,"","the server is healthy"))
+    res.status(200).json(new ApiResponse(200,"","the server is healthy"))
 })
 
 export {heatlthCheck}
